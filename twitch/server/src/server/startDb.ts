fe85650eb72df27d303662f04026d68bd3587daf
fnyaoo <47155307+fnyaoo@users.noreply.github.com>
2020-04-30 17:07:25 +0300

    Delete startDb.ts

--- twitch/server/src/server/startDb.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const MONGO = process.env.MONGO;
-
-export const startDb = (): Promise<typeof import("mongoose")> => mongoose.connect(MONGO || '', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-});
